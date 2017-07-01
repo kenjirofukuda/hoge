00f6a4770eb509c60c71f7fb9f9c96e5412907b5
kenjirofukuda <kenjirofukuda@gmail.com>
2017-07-01 18:20:53 +0900

    Add TGraphicView
    move event handler from TFrame to TGraphicView.

---
+++ ugraphicview.pp
@@ -0,0 +1,223 @@
+unit UGraphicView;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Types, UGeometyUtils, UDocument,
+  Controls, Menus, ExtCtrls, ComCtrls,
+  Dialogs, LCLIntf, UPointsDrawer;
+
+type
+  TTrackingAttributes = record
+    MiddleDown: boolean;
+    CurrPoint: TPoint;
+    DownPoint: TPoint;
+    MovePoints: THVPointList;
+  end;
+
+  TGraphicView = class(TPaintBox)
+    constructor Create(AOwner: TComponent); override;
+
+    procedure HandleMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: integer);
+
+
+    procedure Paint; override;
+    procedure DoOnResize; override;
+
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState;
+      X, Y: integer); override;
+    procedure MouseMove(Shift: TShiftState; X, Y: integer); override;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: integer); override;
+    function DoMouseWheel(Shift: TShiftState; WheelDelta: integer;
+      MousePos: TPoint): boolean; override;
+
+  private
+    FDocument: TDocument;
+    FPointsDrawer: TPointsDrawer;
+    FTrackingAttributes: TTrackingAttributes;
+    FFirstResizeHandled: boolean;
+    procedure HandleMouseMove(Sender: TObject; Shift: TShiftState; X, Y: integer);
+    procedure HandleMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: integer);
+    procedure HandleMouseWheel(Sender: TObject; Shift: TShiftState;
+      WheelDelta: integer; MousePos: TPoint; var Handled: boolean);
+    procedure HandlePaint(Sender: TObject);
+    procedure HandleResize(Sender: TObject);
+
+  public
+    destructor Destroy; override;
+    property Document: TDocument read FDocument write FDocument;
+    property PointsDrawer: TPointsDrawer read FPointsDrawer write FPointsDrawer;
+  end;
+
+implementation
+
+
+constructor TGraphicView.Create(AOwner: TComponent);
+begin
+  inherited Create(AOwner);
+end;
+
+
+destructor TGraphicView.Destroy;
+begin
+  FreeAndNil(FTrackingAttributes.MovePoints);
+  inherited Destroy;
+end;
+
+
+procedure TGraphicView.HandleMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: integer);
+begin
+  if not Assigned(FPointsDrawer) then
+    exit;
+  if Button = mbMiddle then
+  begin
+    with FTrackingAttributes do
+    begin
+      MiddleDown := True;
+      DownPoint := Point(X, Y);
+      if MovePoints = nil then
+        MovePoints := THVPointList.Create;
+      MovePoints.Clear;
+    end;
+  end;
+end;
+
+
+procedure TGraphicView.HandleMouseMove(Sender: TObject; Shift: TShiftState;
+  X, Y: integer);
+var
+  p1, p2: TPoint;
+  wp1, wp2, moved: TPointF;
+begin
+  //UpdateMouseStatus(X, Y);
+  if not Assigned(FPointsDrawer) then
+    exit;
+  with FTrackingAttributes do
+  begin
+    if not MiddleDown then
+      exit;
+    if DownPoint = Point(X, Y) then
+      exit;
+    CurrPoint := Point(X, Y);
+    MovePoints.Add(CurrPoint);
+    if MovePoints.Count > 2 then
+    begin
+      p1 := MovePoints.Items[MovePoints.Count - 2];
+      p2 := MovePoints.Items[MovePoints.Count - 1];
+      with FPointsDrawer.Viewport do
+      begin
+        wp1 := DeviceToWorld(p1.x, p1.y);
+        wp2 := DeviceToWorld(p2.x, p2.y);
+      end;
+      moved := wp2 - wp1;
+      FPointsDrawer.Viewport.OffSetWorldCenter(-moved.x, -moved.y);
+      Invalidate;
+    end;
+  end;
+end;
+
+
+procedure TGraphicView.HandleMouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: integer);
+var
+  xyPoint: TPointF;
+begin
+  if not Assigned(FPointsDrawer) then
+    exit;
+  if not Assigned(FDocument) then
+    exit;
+  if Button = mbLeft then
+  begin
+    xyPoint := FPointsDrawer.Viewport.DeviceToWorld(X, Y);
+    FDocument.AddPoint(xyPoint.x, xyPoint.y);
+  end;
+  FTrackingAttributes.MiddleDown := False;
+end;
+
+
+procedure TGraphicView.HandleMouseWheel(Sender: TObject; Shift: TShiftState;
+  WheelDelta: integer; MousePos: TPoint; var Handled: boolean);
+var
+  direction: single;
+begin
+  if not Assigned(FPointsDrawer) then
+    exit;
+  if WheelDelta < 0 then
+    direction := -1.0
+  else
+    direction := 1.0;
+  FPointsDrawer.Viewport.WheelZoom(MousePos.x, MousePos.y, direction);
+  Invalidate;
+end;
+
+
+procedure TGraphicView.HandlePaint(Sender: TObject);
+begin
+  if not Assigned(FPointsDrawer) then
+    exit;
+  FPointsDrawer.DrawOn(Canvas);
+end;
+
+
+procedure TGraphicView.HandleResize(Sender: TObject);
+begin
+  if not Assigned(FPointsDrawer) then
+    exit;
+  FPointsDrawer.Viewport.SetPortSize(ClientWidth, ClientHeight);
+  if not FFirstResizeHandled then
+  begin
+    FPointsDrawer.Viewport.ResetPortCenter;
+    FFirstResizeHandled := True;
+  end;
+end;
+
+
+procedure TGraphicView.MouseDown(Button: TMouseButton; Shift: TShiftState;
+  X, Y: integer);
+begin
+  inherited MouseDown(Button, Shift, X, Y);
+  HandleMouseDown(self, Button, Shift, X, Y);
+end;
+
+
+procedure TGraphicView.MouseMove(Shift: TShiftState; X, Y: integer);
+begin
+  inherited MouseMove(Shift, X, Y);
+  HandleMouseMove(self, Shift, X, Y);
+end;
+
+
+procedure TGraphicView.MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: integer);
+begin
+  inherited MouseUp(Button, Shift, X, Y);
+  HandleMouseUp(self, Button, Shift, X, Y);
+end;
+
+
+function TGraphicView.DoMouseWheel(Shift: TShiftState; WheelDelta: integer;
+  MousePos: TPoint): boolean;
+begin
+  Result := inherited DoMouseWheel(Shift, WheelDelta, MousePos);
+  HandleMouseWheel(self, Shift, WheelDelta, MousePos, Result);
+end;
+
+
+procedure TGraphicView.Paint;
+begin
+  HandlePaint(self);
+  inherited Paint;
+end;
+
+
+procedure TGraphicView.DoOnResize;
+begin
+  inherited DoOnResize;
+  HandleResize(Self);
+end;
+
+end.
--- umainform.pp
+++ umainform.pp
@@ -5,19 +5,11 @@ unit UMainForm;
 interface
 
 uses
-  Classes, SysUtils, Types, fgl, UGeometyUtils, UDocument,
+  Classes, SysUtils, Types, UGeometyUtils, UDocument,
   Forms, Controls, Menus, ExtCtrls, ComCtrls,
-  Dialogs, LCLIntf, ActnList, UPointsDrawer;
+  Dialogs, LCLIntf, ActnList, UPointsDrawer, UGraphicView;
 
 type
-
-  TTrackingAttributes = record
-    MiddleDown: boolean;
-    CurrPoint: TPoint;
-    DownPoint: TPoint;
-    MovePoints: THVPointList;
-  end;
-
   { TMainForm }
 
   TMainForm = class(TForm)
@@ -31,7 +23,6 @@ type
     ShowAxisLineAction: TAction;
     ActionList: TActionList;
     MainMenu: TMainMenu;
-    PointsView: TPaintBox;
     StatusBar: TStatusBar;
 
     EditMenu: TMenuItem;
@@ -41,17 +32,10 @@ type
 
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
-    procedure PointsViewMouseDown(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: integer);
 
-    procedure PointsViewMouseMove(Sender: TObject; Shift: TShiftState;
+    procedure GraphicViewMouseMove(Sender: TObject; Shift: TShiftState;
       X, Y: integer);
-    procedure PointsViewMouseUp(Sender: TObject; Button: TMouseButton;
-      Shift: TShiftState; X, Y: integer);
-    procedure PointsViewMouseWheel(Sender: TObject; Shift: TShiftState;
-      WheelDelta: integer; MousePos: TPoint; var Handled: boolean);
-    procedure PointsViewPaint(Sender: TObject);
-    procedure PointsViewResize(Sender: TObject);
+    procedure GraphicViewResize(Sender: TObject);
 
     procedure ClearAllMenuItemClick(Sender: TObject);
     procedure RevealAppConfigDirMenuItemClick(Sender: TObject);
@@ -63,8 +47,7 @@ type
   private
     FDocument: TDocument;
     FPointsDrawer: TPointsDrawer;
-    FTrackingAttributes: TTrackingAttributes;
-    FFirstResizeHandled: boolean;
+    FGraphicView: TGraphicView;
     procedure DocumentChange(Sender: TObject);
     procedure UpdateMouseStatus(H, V: integer);
   public
@@ -87,9 +70,24 @@ begin
   FDocument.LoadFromDefault;
   FPointsDrawer := TPointsDrawer.Create(FDocument);
   FDocument.OnChange := @DocumentChange;
-  FTrackingAttributes.MovePoints := THVPointList.Create;
   ClearAllMenuItem.Enabled := FDocument.ClearAllAction.Enabled;
-  FFirstResizeHandled := False;
+
+  FGraphicView := TGraphicView.Create(self);
+  with FGraphicView do
+  begin
+    Document := FDocument;
+    PointsDrawer := FPointsDrawer;
+    AnchorSideLeft.Control := Self;
+    AnchorSideTop.Control := Self;
+    AnchorSideRight.Control := Self;
+    AnchorSideRight.Side := asrBottom;
+    AnchorSideBottom.Control := StatusBar;
+    Anchors := [akTop, akLeft, akRight, akBottom];
+    Visible := True;
+    Parent := Self;
+    OnMouseMove := @GraphicViewMouseMove;
+    OnResize := @GraphicViewResize;
+  end;
 end;
 
 
@@ -104,103 +102,20 @@ begin
   FDocument.SaveToDefault;
   FreeAndNil(FPointsDrawer);
   FreeAndNil(FDocument);
-  FreeAndNil(FTrackingAttributes.MovePoints);
 end;
 
 
-procedure TMainForm.PointsViewMouseDown(Sender: TObject; Button: TMouseButton;
-  Shift: TShiftState; X, Y: integer);
-begin
-  if Button = mbMiddle then
-  begin
-    with FTrackingAttributes do
-    begin
-      MiddleDown := True;
-      DownPoint := Point(X, Y);
-      if MovePoints = nil then
-        MovePoints := THVPointList.Create;
-      MovePoints.Clear;
-    end;
-  end;
-end;
-
-
-procedure TMainForm.PointsViewMouseMove(Sender: TObject; Shift: TShiftState;
+procedure TMainForm.GraphicViewMouseMove(Sender: TObject; Shift: TShiftState;
   X, Y: integer);
-var
-  p1, p2: TPoint;
-  wp1, wp2, moved: TPointF;
 begin
   UpdateMouseStatus(X, Y);
-  with FTrackingAttributes do
-  begin
-    if not MiddleDown then
-      exit;
-    if DownPoint = Point(X, Y) then
-      exit;
-    CurrPoint := Point(X, Y);
-    MovePoints.Add(CurrPoint);
-    if MovePoints.Count > 2 then
-    begin
-      p1 := MovePoints.Items[MovePoints.Count - 2];
-      p2 := MovePoints.Items[MovePoints.Count - 1];
-      with FPointsDrawer.Viewport do
-      begin
-        wp1 := DeviceToWorld(p1.x, p1.y);
-        wp2 := DeviceToWorld(p2.x, p2.y);
-      end;
-      moved := wp2 - wp1;
-      FPointsDrawer.Viewport.OffSetWorldCenter(-moved.x, -moved.y);
-      PointsView.Invalidate;
-    end;
-  end;
 end;
 
 
-procedure TMainForm.PointsViewMouseUp(Sender: TObject; Button: TMouseButton;
-  Shift: TShiftState; X, Y: integer);
-var
-  xyPoint: TPointF;
-begin
-  if Button = mbLeft then
-  begin
-    xyPoint := FPointsDrawer.Viewport.DeviceToWorld(X, Y);
-    FDocument.AddPoint(xyPoint.x, xyPoint.y);
-  end;
-  FTrackingAttributes.MiddleDown := False;
-end;
-
-
-procedure TMainForm.PointsViewMouseWheel(Sender: TObject; Shift: TShiftState;
-  WheelDelta: integer; MousePos: TPoint; var Handled: boolean);
-var
-  direction: single;
-begin
-  if WheelDelta < 0 then
-    direction := -1.0
-  else
-    direction := 1.0;
-  FPointsDrawer.Viewport.WheelZoom(MousePos.x, MousePos.y, direction);
-  PointsView.Invalidate;
-end;
-
-
-procedure TMainForm.PointsViewPaint(Sender: TObject);
-begin
-  FPointsDrawer.DrawOn(PointsView.Canvas);
-end;
-
-
-procedure TMainForm.PointsViewResize(Sender: TObject);
+procedure TMainForm.GraphicViewResize(Sender: TObject);
 begin
   StatusBar.SimpleText := Format('W: %4d, H: %4d',
-    [PointsView.ClientWidth, PointsView.ClientHeight]);
-  FPointsDrawer.Viewport.SetPortSize(PointsView.ClientWidth, PointsView.ClientHeight);
-  if not FFirstResizeHandled then
-  begin
-    FPointsDrawer.Viewport.ResetPortCenter;
-    FFirstResizeHandled := True;
-  end;
+    [FGraphicView.ClientWidth, FGraphicView.ClientHeight]);
 end;
 
 
@@ -213,7 +128,7 @@ end;
 procedure TMainForm.ShowAxisLineActionExecute(Sender: TObject);
 begin
   FPointsDrawer.ShowAxisLine := not FPointsDrawer.ShowAxisLine;
-  PointsView.Refresh;
+  FGraphicView.Invalidate;
 end;
 
 
@@ -226,14 +141,14 @@ end;
 procedure TMainForm.ShowExtentBoundsActionExecute(Sender: TObject);
 begin
   FPointsDrawer.ShowExtentBounds := not FPointsDrawer.ShowExtentBounds;
-  PointsView.Refresh;
+  FGraphicView.Refresh;
 end;
 
 
 procedure TMainForm.ViewFitActionExecute(Sender: TObject);
 begin
   FPointsDrawer.Viewport.SetWorldBounds(FDocument.Bounds);
-  PointsView.Refresh;
+  FGraphicView.Refresh;
 end;
 
 
@@ -248,7 +163,8 @@ begin
   strs.QuoteChar := ' ';
   strs.Add(Format('H: %4d, V: %4d', [H, V]));
   strs.Add(Format('X: %10.4f, Y: %10.4f', [pt.x, pt.y]));
-  strs.Add(Format('W: %4d, H: %4d', [PointsView.ClientWidth, PointsView.ClientHeight]));
+  strs.Add(Format('W: %4d, H: %4d', [FGraphicView.ClientWidth,
+    FGraphicView.ClientHeight]));
   StatusBar.SimpleText := strs.DelimitedText;
   strs.Free;
 end;
@@ -256,7 +172,7 @@ end;
 
 procedure TMainForm.DocumentChange(Sender: TObject);
 begin
-  PointsView.Invalidate;
+  FGraphicView.Invalidate;
   ClearAllMenuItem.Enabled := FDocument.ClearAllAction.Enabled;
 end;
 
